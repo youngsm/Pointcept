0949a8837f8e8605f26ddc8e66a3894804295779
Sam Young <youngsam@stanford.edu>
2025-05-11 12:34:06 -0700

    update config -- 4 node configuration!

--- configs/pilarnet/pretrain-sonata-pilarnet.py
+++ configs/pilarnet/pretrain-sonata-pilarnet.py
@@ -7,8 +7,8 @@ This config file adapts the SONATA pretraining approach for particle physics poi
 _base_ = ["../_base_/default_runtime.py"]
 
 # misc custom setting
-batch_size = 24  # bs: total bs in all gpus
-num_worker = 8
+batch_size = 64 * 4  # bs: total bs in all gpus
+num_worker = 24
 mix_prob = 0
 clip_grad = 3.0
 empty_cache = False
@@ -20,8 +20,9 @@ find_unused_parameters = False
 # Weights & Biases specific settings
 use_wandb = True  # Enable Weights & Biases logging
 wandb_project = "Pretraining-Sonata-PILArNet-M"  # Change to your desired project name
-wandb_run_name = "sonata-pilarnet-pretrain-v1-4GPU"  # Descriptive name for this run
+wandb_run_name = "sonata-pilarnet-pretrain-v6-4GPU-1m-256patch"  # Descriptive name for this run
 
+grid_size = 0.001  # ~ 0.001/(1 / (768.0 * 3**0.5 / 2))
 
 # model settings
 model = dict(
@@ -30,16 +31,16 @@ model = dict(
     backbone=dict(
         type="PT-v3m2",
         in_channels=4,  # [xyz, energy]
-        order=("z", "z-trans", "hilbert", "hilbert-trans"),
+        order=("morton", "morton-trans"),
         stride=(2, 2, 2, 2),
         enc_depths=(3, 3, 3, 9, 3),
         enc_channels=(48, 96, 192, 384, 512),
         enc_num_head=(3, 6, 12, 24, 32),
-        enc_patch_size=(512, 512, 512, 512, 512),
+        enc_patch_size=(256, 256, 256, 256, 256),
         mlp_ratio=4,
         qkv_bias=True,
         qk_scale=None,
-        attn_drop=0.0,
+        attn_drop=0.0, 
         proj_drop=0.0,
         drop_path=0.3,
         shuffle_orders=True,
@@ -69,7 +70,7 @@ model = dict(
     mask_ratio_start=0.3,
     mask_ratio_base=0.7,
     mask_ratio_warmup_ratio=0.05,
-    mask_jitter=0.01,
+    mask_jitter=grid_size / 2, # usually grid_size / 2
     teacher_temp_start=0.04,
     teacher_temp_base=0.07,
     teacher_temp_warmup_ratio=0.05,
@@ -86,7 +87,7 @@ model = dict(
 
 # scheduler settings
 epoch = 60
-base_lr = 0.004 / 4
+base_lr = 0.0026666666666666666
 lr_decay = 0.9  # layer-wise lr decay
 
 base_wd = 0.04  # wd scheduler enable in hooks
@@ -113,10 +114,12 @@ scheduler = dict(
     final_div_factor=1000.0,
 )
 
+
 # dataset settings
 transform = [
-    dict(type="NormalizePILArNet"),
-    dict(type="GridSample", grid_size=0.001, hash_type="fnv", mode="train"),
+    dict(type="NormalizeCoord", center=[384.0, 384.0, 384.0], scale=768.0 * 3**0.5 / 2),
+    dict(type="LogTransform", min_val=1.0e-2, max_val=20.0),
+    dict(type="GridSample", grid_size=grid_size, hash_type="fnv", mode="train"),
     dict(type="Copy", keys_dict={"coord": "origin_coord"}),
     dict(
         type="MultiViewGenerator",
@@ -125,26 +128,27 @@ transform = [
         global_view_scale=(0.4, 1.0),
         local_view_num=4,
         local_view_scale=(0.1, 0.4),
-        global_shared_transform=[
-        ],
+        global_shared_transform=[],
         global_transform=[
-            dict(type="CenterShift", apply_z=True),
+            # dict(type="CenterShift", apply_z=True),
             dict(type="RandomRotate", angle=[-1, 1], axis="z", center=[0, 0, 0], p=0.8),
             dict(type="RandomRotate", angle=[-1, 1], axis="x", center=[0, 0, 0], p=0.8),
             dict(type="RandomRotate", angle=[-1, 1], axis="y", center=[0, 0, 0], p=0.8),
             dict(type="RandomFlip", p=0.5),
+            dict(type="RandomJitter", sigma=grid_size / 4, clip=grid_size),
         ],
         local_transform=[
-            dict(type="CenterShift", apply_z=True),
+            # dict(type="CenterShift", apply_z=True),
             dict(type="RandomRotate", angle=[-1, 1], axis="z", center=[0, 0, 0], p=0.8),
             dict(type="RandomRotate", angle=[-1, 1], axis="x", center=[0, 0, 0], p=0.8),
             dict(type="RandomRotate", angle=[-1, 1], axis="y", center=[0, 0, 0], p=0.8),
             dict(type="RandomFlip", p=0.5),
+            dict(type="RandomJitter", sigma=grid_size / 4, clip=grid_size),
         ],
-        max_size=65536,
+        max_size=30000,
     ),
     dict(type="ToTensor"),
-    dict(type="Update", keys_dict={"grid_size": 0.001}),
+    dict(type="Update", keys_dict={"grid_size": grid_size}),
     dict(
         type="Collect",
         keys=(
@@ -157,6 +161,7 @@ transform = [
             "local_energy",
             "local_offset",
             "grid_size",
+            "segment",
             "name",
         ),
         offset_keys_dict=dict(),
@@ -172,19 +177,25 @@ data = dict(
             # PILArNet
             dict(
                 type="PILArNetH5Dataset",
-                split=["train", "val"],
+                split="train",
                 data_root="/sdf/home/y/youngsam/data/dune/larnet/h5/DataAccessExamples/",
                 transform=transform,
                 test_mode=False,
-                emin=1.0e-2,
-                emax=20.0,
-                energy_threshold=0.0,
+                energy_threshold=0.13,
                 min_points=1024,
-                remove_low_energy_scatters=True,
+                max_len=-1,
+                remove_low_energy_scatters=False,
                 loop=1,
             ),
         ],
-    )
+    ),
+    val=dict(
+        type="PILArNetH5Dataset",
+        split="val",
+        data_root="/sdf/home/y/youngsam/data/dune/larnet/h5/DataAccessExamples/",
+        transform=transform,
+        test_mode=False,
+    ),
 )
 
 hooks = [
@@ -193,5 +204,6 @@ hooks = [
     dict(type="WeightDecaySchedular", base_value=base_wd, final_value=final_wd),
     dict(type="IterationTimer", warmup_iter=2),
     dict(type="InformationWriter"),
-    dict(type="CheckpointSaver", save_freq=5),
+    dict(type="CheckpointSaverIteration", save_freq=2500),
+    # dict(type="PretrainEvaluator", write_cls_iou=True),
 ] 
